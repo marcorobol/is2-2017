eb92895598d974e37e4670ea9e3cb097b863982c
Marco Robol <marco89mr@gmail.com>
2017-09-13 12:50:58 +0200

    javascript intro

--- 1-Javascript-Intro/12-buttons/script.js
+++ 1-Javascript-Intro/12-buttons/script.js
@@ -1,14 +1,6 @@
 var car = {
     type:"Fiat", 
     model:"500", 
-    color:"white"
-};
-
-
-
-var car = {
-    type:"Fiat", 
-    model:"500", 
     color:"white",
     description : function() {
         return this.color + ", " + this.model + ", " + this.type;
